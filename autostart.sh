67a52766d98175741c09e6c3096d441f7acc4f0a
dicekillsyou161 <10100001SEA@protonmail.com>
2022-06-04 01:54:45 -0700

    update wallpaper start script

--- autostart.sh
+++ autostart.sh
@@ -2,10 +2,11 @@
 
 ./.screenlayout/default.sh
 #screen -d -m ff
-screen -d -m feh --bg-fill .config/qtile/wallpaper.jpg
+screen -d -m feh --bg-fill .config/qtile/MDN-MDC-1312.jpg
 screen -d -m flameshot
 screen -d -m yakuake
-screen -d -m volumeicon 
+screen -d -m volumeicon
+wal -R
 #screen -d -m dc
 #xscreensaver-command --restart
 #xscreensaver -nosplash
