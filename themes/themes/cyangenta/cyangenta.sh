b0dd5b203616c4df14adfc4aa5f51d26460c8b87
dicekillsyou161 <10100001SEA@protonmail.com>
2022-06-04 14:15:33 -0700

    Actually include the themes this time

---
+++ themes/themes/cyangenta/cyangenta.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wal -i /home/zorthesosen/.config/qtile/themes/cyangenta/MDN-MDC-1312-Theme-Cyangenta.jpg
+screen -d -m feh --bg-fill /home/zorthesosen/.config/qtile/themes/cyangenta/MDN-MDC-1312-v2.jpg
---
+++ themes/themes/forest/forest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wal -i /home/zorthesosen/.config/qtile/themes/forest/MDN-MDC-1312-Theme-Forest.jpg
+screen -d -m feh --bg-fill /home/zorthesosen/.config/qtile/themes/forest/MDN-MDC-1312-v2-Forest.jpg
