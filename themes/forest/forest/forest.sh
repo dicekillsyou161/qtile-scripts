dcb825ade3bbd5f355e0cccb84c3378138294033
dicekillsyou161 <10100001SEA@protonmail.com>
2022-06-04 14:34:01 -0700

    fuck

--- themes/forest/forest/forest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wal -i /home/zorthesosen/.config/qtile/themes/forest/MDN-MDC-1312-Theme-Forest.jpg
-screen -d -m feh --bg-fill /home/zorthesosen/.config/qtile/themes/forest/MDN-MDC-1312-v2-Forest.jpg
