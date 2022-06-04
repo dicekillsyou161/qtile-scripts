d9df9aa7e7972758b50ca4d4b49bd66e2ecf4654
dicekillsyou161 <10100001SEA@protonmail.com>
2022-06-04 14:27:48 -0700

    delete this shit again. A A A  enter the commit message for your changes. Lines starting

--- themes/themes/cyangenta/cyangenta.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wal -i /home/zorthesosen/.config/qtile/themes/cyangenta/MDN-MDC-1312-Theme-Cyangenta.jpg
-screen -d -m feh --bg-fill /home/zorthesosen/.config/qtile/themes/cyangenta/MDN-MDC-1312-v2.jpg
