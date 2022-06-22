5e6706da1428904b73641378932d42c98941bb85
dicekillsyou161 <dicekillsyou@gmail.com>
2022-06-21 22:25:09 -0700

    add picom autostart & fix the background fill set default

--- vm/autostart.sh
+++ vm/autostart.sh
@@ -7,7 +7,9 @@ screen -d -m yakuake
 screen -d -m volumeicon
 wal -R
 
-screen -d -m feh --bg-fill Pictures/MDN-MDC-1312-v2.jpg
+feh --bg-fill .config/qtile/themes/cyangenta/MDN-MDC-1312-v2.jpg
 #screen -d -m dc
 #xscreensaver-command --restart
 #xscreensaver -nosplash
+
+picom -b
