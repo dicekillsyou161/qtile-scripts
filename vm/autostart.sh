9a0043cad4836635c1b14319369d94f21980ae1c
zorthesosen <dicekillsyou@gmail.com>
2022-07-13 18:56:58 -0700

    Finish integrating the new GPU VM

--- vm/autostart.sh
+++ vm/autostart.sh
@@ -6,7 +6,11 @@ screen -d -m yakuake
 screen -d -m volumeicon
 wal -R                                                                                #set the last used theme settings from pywal's cache
 
+screen -d -m radeon-profile
+
 current_wallpaper=$(</home/zorthesosen/.config/qtile/themes/current_theme_wallpaper)  #Read the path of the last loaded wallpaper into a variable
 feh --bg-fill $current_wallpaper                                                      #re-set the wallpaper after a reboot
 
 picom -b                                                      #use picom for fancy UI elements (such as transparency)
+
+wal -R
